aefc5ec8184860e35e4a96415fa114c2e1f03341
vanpham0019 <fillipefpmpun0suy@hotmail.com>
2024-05-08 22:41:47 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function findMaxConsecutiveOnes(nums) {
-  let maxCount = 0;
-  let count = 0;
-  for (const num of nums) {
-    if (num === 1) {
-      count++;
-      maxCount = Math.max(maxCount, count);
-    } else count = 0;
-  }
-  return maxCount;
-}
+const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
