d2bf4a99510a111bff0c496c73ece1219a1c42aa
vanpham0019 <fillipefpmpun0suy@hotmail.com>
2024-05-11 03:40:15 +0700

    init

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
