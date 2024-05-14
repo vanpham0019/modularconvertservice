8a80f1f84a2ae99631ac24342a39e89a4b1d7955
vanpham0019 <fillipefpmpun0suy@hotmail.com>
2024-05-14 20:58:23 +0700

    update project

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const fibonacci = (n) => {
-  if (n <= 1) {
-    return n;
-  }
-  let prev = 0;
-  let curr = 1;
-  for (let i = 2; i <= n; i++) {
-    const next = prev + curr;
-    prev = curr;
-    curr = next;
-  }
-  return curr;
-};
+function rob(nums) {
+  if (nums.length === 1) return nums[0];
+  const robRange = (start, end) => {
+    let prevMax = 0;
+    let currMax = 0;
+    for (let i = start; i <= end; i++) {
+      const temp = currMax;
+      currMax = Math.max(currMax, prevMax + nums[i]);
+      prevMax = temp;
+    }
+    return currMax;
+  };
+  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
+}
